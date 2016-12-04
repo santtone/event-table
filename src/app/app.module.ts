3f42dc695aa3a99bcb0d5d41daf948a61507a0d7
santtone <sami.anttonen@fifthelement.fi>
2016-12-04 20:31:47 +0200

    Added Polymer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule  } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { PolymerElement } from '@vaadin/angular2-polymer';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PolymerElement('paper-drawer-panel')
   ],
   imports: [
     BrowserModule,
