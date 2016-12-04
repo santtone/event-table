03820d330b6ae1ec28ab643d3a0535cd521c856b
santtone <sami.anttonen@fifthelement.fi>
2016-12-05 00:20:25 +0200

    Some wheel stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, AfterViewInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements AfterViewInit {
   title = 'app works!';
+
+  months = [
+    {id: '1', name: 'January', color: 'rgba(0,0,0,0)'},
+    {id: '2', name: 'February', color: 'rgba(0,0,0,0.1)'},
+    {id: '3', name: 'March', color: 'rgba(0,0,0,0.2)'},
+    {id: '4', name: 'April', color: 'rgba(0,0,0,0.3)'},
+    {id: '5', name: 'May', color: 'rgba(0,0,0,0.4)'},
+    {id: '6', name: 'June', color: 'rgba(0,0,0,0.5)'},
+    {id: '7', name: 'July', color: 'rgba(0,0,0,0.6)'},
+    {id: '8', name: 'August', color: 'rgba(0,0,0,0.7)'},
+    {id: '9', name: 'September', color: 'rgba(0,0,0,0.8)'},
+    {id: '10', name: 'October', color: 'rgba(0,0,0,0.9)'},
+    {id: '11', name: 'November', color: 'rgba(0,0,0,0.95)'},
+    {id: '12', name: 'December', color: 'rgba(0,0,0,1)'}
+  ];
+
+  ngAfterViewInit(): void {
+  }
 }
