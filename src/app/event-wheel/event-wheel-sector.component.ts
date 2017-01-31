5b5eac818cc3a764c4eb03f2701b2165a85b7fcd
santtone <sami.anttonen@trimble.com>
2017-01-31 18:52:26 +0200

    Implemented the sector component

--- src/app/event-wheel/event-wheel-sector.component.ts
+++ src/app/event-wheel/event-wheel-sector.component.ts
@@ -1,4 +1,4 @@
-import {Component, AfterViewInit, OnInit} from '@angular/core';
+import {Component, OnInit, ElementRef, Input} from '@angular/core';
 
 @Component({
   selector: 'ew-event-wheel-sector',
@@ -7,29 +7,50 @@ import {Component, AfterViewInit, OnInit} from '@angular/core';
 
 export class EventWheelSector implements OnInit {
 
+  @Input() radius: number;
+  @Input() startAngle: number;
+  @Input() endAngle: number;
+  @Input() index: number;
+
   pathD = '';
+  element = null;
+  sectorSize = 0;
+  initialize;
+
+  constructor(el: ElementRef) {
+    this.element = el;
+
+    this.initialize = function (radius, start, end) {
+      function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+        const angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+        return {
+          x: centerX + (radius * Math.cos(angleInRadians)),
+          y: centerY + (radius * Math.sin(angleInRadians))
+        };
+      }
+
+      function describeArc(x, y, radius, startAngle, endAngle) {
+        const start = polarToCartesian(x, y, radius, endAngle);
+        const end = polarToCartesian(x, y, radius, startAngle);
+        const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+        return [
+          "M", start.x, start.y,
+          "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
+        ].join(" ");
+      }
+
+      this.sectorSize = 2 * (radius + 100);
+      this.pathD = describeArc((radius + 100), (radius + 100), radius, start, end);
+      console.log((radius + 100) + ',' + (radius + 100) + ',' + radius + ',' + start + ',' + end)
+    }
+  }
 
   ngOnInit(): void {
+    this.initialize(this.radius, this.startAngle, this.endAngle);
+  }
 
-    function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
-      const angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
-      return {
-        x: centerX + (radius * Math.cos(angleInRadians)),
-        y: centerY + (radius * Math.sin(angleInRadians))
-      };
-    }
-
-    function describeArc(x, y, radius, startAngle, endAngle) {
-      const start = polarToCartesian(x, y, radius, endAngle);
-      const end = polarToCartesian(x, y, radius, startAngle);
-      const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
-      return [
-        "M", start.x, start.y,
-        "A", radius, radius, 0, largeArcFlag, 0, end.x, end.y
-      ].join(" ");
-    }
-
-    this.pathD = describeArc(330, 385, 230, 0, 300);
+  selected(index) {
+    console.log('sector ' + index + ' selected');
   }
 
 }
--- src/app/event-wheel/event-wheel.component.ts
+++ src/app/event-wheel/event-wheel.component.ts
@@ -1,4 +1,5 @@
-import {Component, AfterViewInit, OnInit} from '@angular/core';
+import {Component, OnInit, ElementRef} from '@angular/core';
+import {EventWheelSector} from "./event-wheel-sector.component";
 
 @Component({
   selector: 'ew-event-wheel',
@@ -6,29 +7,34 @@ import {Component, AfterViewInit, OnInit} from '@angular/core';
   styleUrls: ['./event-wheel.component.css']
 })
 
-export class EventWheel implements OnInit, AfterViewInit {
+export class EventWheel implements OnInit {
 
-  months = [
-    {id: '1', name: 'January', color: 'rgba(0,0,0,0)'},
-    {id: '2', name: 'February', color: 'rgba(0,0,0,0.1)'},
-    {id: '3', name: 'March', color: 'rgba(0,0,0,0.2)'},
-    {id: '4', name: 'April', color: 'rgba(0,0,0,0.3)'},
-    {id: '5', name: 'May', color: 'rgba(0,0,0,0.4)'},
-    {id: '6', name: 'June', color: 'rgba(0,0,0,0.5)'},
-    {id: '7', name: 'July', color: 'rgba(0,0,0,0.6)'},
-    {id: '8', name: 'August', color: 'rgba(0,0,0,0.7)'},
-    {id: '9', name: 'September', color: 'rgba(0,0,0,0.8)'},
-    {id: '10', name: 'October', color: 'rgba(0,0,0,0.9)'},
-    {id: '11', name: 'November', color: 'rgba(0,0,0,0.95)'},
-    {id: '12', name: 'December', color: 'rgba(0,0,0,1)'}
-  ];
-
+  element = null;
+  center: number[];
+  months: any[];
   wheelRotation = 0;
+
+  constructor(el: ElementRef) {
+    this.element = el.nativeElement;
+    this.months = [
+      {id: '1', name: 'January', color: 'rgba(0,0,0,0)'},
+      {id: '2', name: 'February', color: 'rgba(0,0,0,0.1)'},
+      {id: '3', name: 'March', color: 'rgba(0,0,0,0.2)'},
+      {id: '4', name: 'April', color: 'rgba(0,0,0,0.3)'},
+      {id: '5', name: 'May', color: 'rgba(0,0,0,0.4)'},
+      {id: '6', name: 'June', color: 'rgba(0,0,0,0.5)'},
+      {id: '7', name: 'July', color: 'rgba(0,0,0,0.6)'},
+      {id: '8', name: 'August', color: 'rgba(0,0,0,0.7)'},
+      {id: '9', name: 'September', color: 'rgba(0,0,0,0.8)'},
+      {id: '10', name: 'October', color: 'rgba(0,0,0,0.9)'},
+      {id: '11', name: 'November', color: 'rgba(0,0,0,0.95)'},
+      {id: '12', name: 'December', color: 'rgba(0,0,0,1)'}
+    ];
+  }
 
   private calculateRotation(): void {
     const d = new Date();
     const days = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
-    console.log(days);
     this.wheelRotation = (d.getMonth() * 30 + 60) - (360 / days);
   }
 
@@ -38,8 +44,9 @@ export class EventWheel implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.calculateRotation();
+    let x = this.element.offsetLeft + this.element.offsetWidth / 2;
+    let y = this.element.offsetTop + this.element.offsetHeight / 2;
+    this.center = [x, y];
   }
 
-  ngAfterViewInit(): void {
-  }
 }
