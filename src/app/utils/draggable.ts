3c392e3a1d3b8d0cd3e648c784694f87fcffc6d9
santtone <sami.anttonen@fifthelement.fi>
2017-02-04 19:06:56 +0200

    Enabled dragging for touch devices

--- src/app/utils/draggable.ts
+++ src/app/utils/draggable.ts
@@ -5,32 +5,42 @@ import {Directive, HostListener, ElementRef, OnInit} from "@angular/core";
 })
 
 export class Draggable implements OnInit {
+
   changeX: number = 0;
   changeY: number = 0;
   element;
-
-  @HostListener('dragstart', ['$event'])
-  onDragStart(event: MouseEvent) {
-    this.changeX = event.x - this.element.nativeElement.offsetLeft;
-    this.changeY = event.y - this.element.nativeElement.offsetTop;
-  }
-
-  @HostListener('drag', ['$event'])
-  onDrag(event: MouseEvent) {
-    this.changePosition(event.x, event.y);
-  }
-
-  @HostListener('dragend', ['$event'])
-  onDragEnd(event: MouseEvent) {
-    this.changeX = 0;
-    this.changeY = 0;
-  }
 
   constructor(element: ElementRef) {
     this.element = element;
   }
 
-  changePosition(x: number, y: number) {
+  @HostListener('touchstart', ['$event'])
+  @HostListener('dragstart', ['$event'])
+  onDragStart(event) {
+    let x = event.touches ? event.touches[0].clientX : event.x;
+    let y = event.touches ? event.touches[0].clientY : event.y;
+    this.changeX = x - this.element.nativeElement.offsetLeft;
+    this.changeY = y - this.element.nativeElement.offsetTop;
+  }
+
+  @HostListener('touchmove', ['$event'])
+  @HostListener('drag', ['$event'])
+  onDrag(event) {
+    if (event.touches) {
+      this.changePosition(event.touches[0].clientX, event.touches[0].clientY);
+    } else {
+      this.changePosition(event.x, event.y);
+    }
+  }
+
+  @HostListener('touchend', ['$event'])
+  @HostListener('dragend', ['$event'])
+  onDragEnd() {
+    this.changeX = 0;
+    this.changeY = 0;
+  }
+
+  private changePosition(x: number, y: number): void {
     if (!x || !y) return;
     this.element.nativeElement.style.top = (y - this.changeY) + 'px';
     this.element.nativeElement.style.left = (x - this.changeX) + 'px';
